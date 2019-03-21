a2111f0be57a36634f08932c76c9c4bfc0ae7c51
subash789 <46813541+subash789@users.noreply.github.com>
2019-03-21 22:48:10 +0530

    Update SET3-27.C

--- SET3-27.C
+++ SET3-27.C
@@ -2,25 +2,25 @@
 
 int main()
 {
-  char s[50];
+  char ss[50];
   int i,j,k,l;
- scanf("%[^\n]s",s);
-  for(i=0;s[i]!='\0';i++)
+ scanf("%[^\n]s",ss);
+  for(i=0;ss[i]!='\0';i++)
   {
-      if(s[i]==' ' && s[i+1]==' ')
+      if(ss[i]==' ' && ss[i+1]==' ')
       {
          k=i+2;
          j=i;
-         for(l=k;s[l]!='\0';l++)
+         for(l=k;ss[l]!='\0';l++)
          {
-             s[j]=s[l];
+             ss[j]=ss[l];
              j++;
          }
-         s[j]='\0';
+         ss[j]='\0';
          break;
       }
   }
-  printf("%s",s);
+  printf("%s",ss);
   return 0;
   
 }
