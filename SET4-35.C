26b1f2387ab08a0b44ddfb2271522c15d98c64f9
subash789 <46813541+subash789@users.noreply.github.com>
2019-03-22 22:56:48 -0700

    Update SET4-35.C

--- SET4-35.C
+++ SET4-35.C
@@ -1,13 +1,4 @@
-#include<stdio.h>
-int main()
-{
-    char s[50];
-    int i=0;
-    scanf("%[^\n]s",s);
-    printf("%c%c",s[i],s[i+3]);
-    return 0;
-}
-///////////////////////////////////////////////////5
+
 #include <stdio.h>
 
 int main()
