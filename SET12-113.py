b4e3bcb0f44ae68bd26b2b43b6506dddec557944
subash789 <46813541+subash789@users.noreply.github.com>
2019-03-26 15:19:47 -0700

    Update SET12-113.py

--- SET12-113.py
+++ SET12-113.py
@@ -1,5 +1,5 @@
 d,m,y=input().split("/")
-if len(d)<=2 and len(m)<=2 and len(y)==4:
+if len(d)==2 and len(m)==2 and len(y)==4:
   if int(d) in range (1,32) and int(m) in range(1,13) and int(y) in range(1,9999):
     print("yes")
   else:
